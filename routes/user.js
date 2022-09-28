1c6d64a2f59527e0e602d7e940e0a58ed3b8cee9
Muhammed Mukthar <muktharmanaf28@gmail.com>
2022-09-28 17:31:29 +0530

    movies has been displayed

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
-
 const ticketController=require('../controller/Booking_ticketController')
-
+const movies=require('../model/movies')
 router.post('/book-ticket',ticketController.book_Ticket)
+
+router.get('/list-movies',async(req,res)=>{
+    let name=req.params.name;
+
+  let all_movies= await movies.find().sort({movie:1})
+   
+  
+   res.status(200).json(all_movies)
+})
 
  
 module.exports = router;
